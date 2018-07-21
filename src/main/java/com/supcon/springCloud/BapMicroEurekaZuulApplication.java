ccb48e1f1079578898e0f50cd072adb38bec1e2e
zhushizhang126 <zhushizhang126@qq.com>
2018-07-21 20:33:21 +0800

    bap-micro-eureka-zuul first commit

---
+++ src/main/java/com/supcon/springCloud/BapMicroEurekaZuulApplication.java
@@ -0,0 +1,16 @@
+package com.supcon.springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class BapMicroEurekaZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapMicroEurekaZuulApplication.class, args);
+	}
+}
